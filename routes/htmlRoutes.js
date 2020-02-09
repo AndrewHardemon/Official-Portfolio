e0a03938afd4a5625f8f81756363007c51f1d1f7
Andrew Hardemon <aeroatlas@ymail.com>
2020-02-09 02:21:03 -0700

    starting the projects page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,6 +9,10 @@ module.exports = function(app) {
   app.get("/about-me", function(req, res) {
     res.render("about");
   });
+
+  app.get("/projects", function(req, res) {
+    res.render("projects");
+  });
   
   app.get("*", function(req, res) {
     res.render("404");
