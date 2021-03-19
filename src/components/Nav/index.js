e668820f352df76707d6ae33a71fc503f0a42d61
Andrew <andrewhardemon@gmail.com>
2021-03-18 23:18:55 -0700

    Added nav background image Created Resume Page (might remove later) Added PDF for download and to embed in resume page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,7 +13,7 @@ function Nav(props){
       <ul className="flex-row">
         {pages?.map(page => (
           <li
-            className={`mx-5 ${currentPage.name === page.name ?? 'navActive'}`}
+            className={`mx-5 ${currentPage.name === page.name ? 'navActive' : ""}`}
             key={page.name}
           >
             <span onClick={() => setCurrentPage(page)}>{capFirstLetter(page.name)}</span>
---
+++ src/components/Resume/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Document, Page } from 'react-pdf';
+
+
+function Resume() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <embed src="../../assets/profile/Andrew-Hardemon-Web-Development-TA-Resume.pdf" type="application/pdf" width="100%" height="100%"></embed>
+
+    </div>
+  )
+}
+
+
+export default Resume;
