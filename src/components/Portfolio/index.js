64a5c3f8acdcaf1cdbdbe85e60236ed8fe41c1cf
AndrewHardemon <andrewhardemon@gmail.com>
2023-02-20 02:46:11 -0500

    Added SAUS project and added disabled feature to Contact

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,7 @@ import vamp from "../../assets/projects/vamp-desktop.png"
 import sfw from "../../assets/projects/safe-for-work.png"
 import recommendations from "../../assets/projects/semi-random-recommendations.png"
 import adventure from "../../assets/projects/text-based-rpg.png"
+import saus from "../../assets/projects/saus_project.png"
 import "./style.css"
 
 function Portfolio(){
@@ -12,10 +13,19 @@ function Portfolio(){
       name: "vamp-desktop",
       description: "Real Time Chat Application",
       technologies: "MERN/socket.io/Electron",
-      link: "https://vamp-desktop.onrender.com/",
-      repo: "https://github.com/AeroAtlas/Vamp-Desktop",
+      link: "https://vamp-desktop.herokuapp.com/",
+      repo: "https://github.com/AndrewHardemon/Vamp-Desktop",
       image: vamp,
       fullDescription: `Real time chat application created with MERN, socket.io, and multer to allowing for instant communication and image sharing.`
+    },
+    {
+      name: "semi-random-recommendations",
+      description: "Random Movie and Game Generator",
+      technologies: "MERN/socket.io/Electron",
+      link: "https://semi-random-recommendations.herokuapp.com/",
+      repo: "https://github.com/AndrewHardemon/Semi-Random-Recommendations",
+      image: recommendations,
+      fullDescription: `Random Movie and Video Game generator. Following MVC patterns with the use of Handlebars and Sequelize/SQL, it allows a user to put in several parameters for both games and movies, so the user can get random results for their specific interests.`
     },
     {
       name: "safe-for-work",
@@ -27,20 +37,20 @@ function Portfolio(){
       fullDescription: `Program for teaching, testing, and graphing the results of employees of a company. Created with MERN and graph.js, it allows for multiple divisions of a company to be tested along side eachother.`
     },
     {
-      name: "semi-random-recommendations",
-      description: "Random Movie and Game Generator",
-      technologies: "MERN/socket.io/Electron",
-      link: "https://semi-random-recommendations.onrender.com/",
-      repo: "https://github.com/AeroAtlas/Semi-Random-Recommendations",
-      image: recommendations,
-      fullDescription: `Random Movie and Video Game generator. Following MVC patterns with the use of Handlebars and Sequelize/SQL, it allows a user to put in several parameters for both games and movies, so the user can get random results for their specific interests.`
+      name: "S.A.U.S",
+      description: "A Stimulating Electron App",
+      technologies: "Electron/Node.js",
+      link: "https://andrewhardemon.github.io/SAUS/",
+      repo: "https://github.com/AndrewHardemon/SAUS",
+      image: saus,
+      fullDescription: `A stimulating electron app designed to have sounds and colors bounce around the window.`
     },
     {
       name: "text-based-rpg",
       description: "React Text Adventure Game",
       technologies: "MERN/GraphQL",
       link: "https://text-based-rpg-atlas.herokuapp.com/",
-      repo: "https://github.com/AeroAtlas/text-based-rpg",
+      repo: "https://github.com/AndrewHardemon/text-based-rpg",
       image: adventure,
       fullDescription: `Text Adventure Game with dynamic stats, choices, and inventory. Uses MERN to allow for instant transitions between each part of the adventure.`
     },
@@ -51,13 +61,13 @@ function Portfolio(){
       <div className="portfolio-container">
         <div className="project-highlight">
           <div className="button-container">
-            <button onClick={() => setProjects(projects.concat(projects.shift()))}>Prev</button>
-            <button onClick={() => setProjects([projects.pop()].concat(projects))}>Next</button>
+            <button onClick={() => setProjects([projects.pop()].concat(projects))}>Prev</button>
+            <button onClick={() => setProjects(projects.concat(projects.shift()))}>Next</button>
           </div>
           <Project project={projects[0]} extra={true}/>
         </div>
         <div className="flex-row project-container">
-        {projects.map((project, index) => (
+        {projects.filter((_,i) => i != 0).map((project, index) => (
           <Project project={project} index={index} key={index}/>
           ))}
         </div>
